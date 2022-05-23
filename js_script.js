5684a5474abf6526f0370121b3152958d7e632dc
GarimaBansal72 <103357469+GarimaBansal72@users.noreply.github.com>
2022-05-23 21:47:16 +0530

    Update js_script.js

---
+++ js_script.js
@@ -0,0 +1,17 @@
+var list = document.querySelector('ul');
+list.addEventListener('click', function(evnt) {
+  if (evnt.target.tagName === 'LI') {
+    evnt.target.classList.toggle('checked');
+  }
+});
+
+function new_task(){
+    var task=document.forms[0].elements[0].value;
+    if (task==""){
+        alert("Cannot create empty task, please enter something.");
+    }
+    else {
+        document.getElementById("list").appendChild(document.createElement("li").append(document.createTextNode(task)));
+    }
+    document.forms[0].elements[0].value = "";
+}
